69fad030e6b0963f1d5a65ff35ef3101fe033b63
unknown <sulpiana.tik@gmail.com>
2022-04-12 17:51:49 +0700

    elevated button

--- main.dart
+++ main.dart
@@ -13,17 +13,27 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           appBar: AppBar(
             title: const Text("Sulpiana"),
+            centerTitle: true,
           ),
           body: Center(
-            child: Container(
-              width: 350,
-              height: 500,
-              color: Colors.white10,
-              child: Image(
-                image: NetworkImage(
-                    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/91/0c/10/caption.jpg?w=300&h=300&s=1"),
-                fit: BoxFit.fill,
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Clik Here",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.lightBlueAccent,
+                    onPrimary: Colors.white,
+                    onSurface: Colors.blueGrey,
+                    shadowColor: Colors.green,
+                    minimumSize: Size(150, 60),
+                  ),
+                ),
+              ],
             ),
           )),
     );
